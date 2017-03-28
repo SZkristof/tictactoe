a2a960d045ddf9da8dddd65e425ad8da7d1f7bc2
SZkristof <szabokristof0@gmail.com>
2017-03-28 11:59:43 +0200

    pep8 debugging

--- tictactoegamepetiandkristof.py
+++ tictactoegamepetiandkristof.py
@@ -3,10 +3,12 @@ import time
 import sys
 from random import uniform
 
+
 def drawBoard(board):
+
     import os
     os.system('clear')
-   
+
     print('\033[1;33m│¯¯¯¯¯││¯¯¯¯¯││¯¯¯¯¯│\033[1;m')
     print('\033[1;33m│  \033[1;m' + board[7] + '\033[1;33m  ││  \033[1;m'    + board[8] +'\033[1;33m  ││  \033[1;m' + board[9] +'\033[1;33m  │\033[1;m')
     print('\033[1;33m│_____││_____││_____│\033[1;m')
@@ -14,13 +16,15 @@ def drawBoard(board):
     print('\033[1;33m│¯¯¯¯¯││¯¯¯¯¯││¯¯¯¯¯│')
     print('\033[1;33m│  \033[1;m' + board[4] + '\033[1;33m  ││  \033[1;m'  + board[5] +'\033[1;33m  ││  \033[1;m' + board[6]+'\033[1;33m  │\033[1;m')
     print('\033[1;33m│_____││_____││_____│\033[1;m')
-   
+
     print('\033[1;33m│¯¯¯¯¯││¯¯¯¯¯││¯¯¯¯¯│\033[1;m')
     print('\033[1;33m│  \033[1;m' + board[1] + '\033[1;33m  ││  \033[1;m'  + board[2] +'\033[1;33m  ││  \033[1;m' + board[3]+'\033[1;33m  │\033[1;m')
     print('\033[1;33m│     ││     ││     │\033[1;m')
     print('\033[1;33m ¯¯¯¯¯  ¯¯¯¯¯  ¯¯¯¯¯\033[1;m')
 
+
 def inputPlayerLetter():
+
     letter = ''
     while not (letter == 'X' or letter == 'O'):
         print('\033[1;33mⓨⓞⓤ ⓦⓘⓛⓛ ⓑⓔ ⓟⓛⓐⓨⓔⓡ ⓞⓝⓔ. ⓓⓞ ⓨⓞⓤ ⓦⓐⓝⓣ ⓣⓞ ⓑⓔ │ⓧ│ ⓞⓡ │ⓩ│?\033[1;m')
@@ -30,6 +34,7 @@ def inputPlayerLetter():
     else:
         return ['\033[1;31mO\033[1;m', '\033[1;34mX\033[1;m']
 
+
 def inputPlayer2Letter():
     if inputPlayerLetter() == "X":
         print ("You will be O")
@@ -38,14 +43,17 @@ def inputPlayer2Letter():
         print ("You are X.")
         return ["X", "\033[1;31mO\033[1;m"]
 
+
 def whoGoesFirst():
     if random.randint(0, 1) == 0:
         return 'player1'
     else:
         return 'player2'
 
+
 def makeMove(board, letter, move):
     board[move] = letter
+
 
 def isWinner(bo, le):
     return ((bo[7] == le and bo[8] == le and bo[9] == le) or
@@ -57,6 +65,7 @@ def isWinner(bo, le):
     (bo[7] == le and bo[5] == le and bo[3] == le) or 
     (bo[9] == le and bo[5] == le and bo[1] == le)) 
 
+
 def getPlayerMove(board):
     move = ' '
     while move not in '1 2 3 4 5 6 7 8 9'.split() or not isSpaceFree(board, int(move)):
@@ -65,6 +74,7 @@ def getPlayerMove(board):
         print('\033[1;33m|/\| |  | /~~\  |     | .__/     |  \__/ \__/ |  \    | \| |___ / \  |      |  | \__/  \/  |___  . \033[1;m')
         move = input()
     return int(move)
+
 
 def getPlayer2Move(board):
     move = ' '
@@ -75,8 +85,10 @@ def getPlayer2Move(board):
         move = input()
     return int(move)
 
+
 def isSpaceFree(board, move):
     return board[move] == ' '
+
 
 def isBoardFull(board):
     for i in range(1, 10):
@@ -110,26 +122,26 @@ while True:
     for x in line_4:
         print(x, end='')
         sys.stdout.flush()
-        time.sleep(0.005)    
+        time.sleep(0.005)
     time.sleep(0.1)
     print("")
     line_5 = ("\033[1;31m   ██║   ██║╚██████╗\033[1;m       ██║   ██║  ██║╚██████╗\033[1;m     \033[1;32m  ██║   ╚██████╔╝███████╗\033[1;m")
     for x in line_5:
         print(x, end='')
         sys.stdout.flush()
-        time.sleep(0.005)    
+        time.sleep(0.005)
     time.sleep(0.1)
     print("")
     line_6 = ("\033[1;31m   ╚═╝   ╚═╝ ╚═════╝\033[1;m       ╚═╝   ╚═╝  ╚═╝ ╚═════╝\033[1;m     \033[1;32m  ╚═╝    ╚═════╝ ╚══════╝\033[1;m")
     for x in line_6:
         print(x, end='')
         sys.stdout.flush()
-        time.sleep(0.005)    
+        time.sleep(0.005)
     time.sleep(2)
     print("")
     print("")
     print("")
-    
+
     theBoard = [' '] * 10
     playerLetter, player2Letter = inputPlayerLetter()
     turn = whoGoesFirst()
@@ -137,11 +149,11 @@ while True:
     gameIsPlaying = True
 
     while gameIsPlaying:
-        if turn =="player1":
+        if turn == "player1":
             drawBoard(theBoard)
             move = getPlayerMove(theBoard)
             makeMove(theBoard, playerLetter, move)
-            
+
             if isWinner(theBoard, playerLetter):
                 drawBoard(theBoard)
                 print('────────────────█████████───────────────')
@@ -171,7 +183,7 @@ while True:
                 print('────────────────────────────────────────')
                 print('────────────────█████████───────────────')
                 print('──────────────█████████████─────────────')
-                
+
                 print('\033[1;33m██╗   ██╗██╗ ██████╗████████╗ ██████╗ ██████╗ ██╗   ██╗\033[1;m')
                 print('\033[1;33m██║   ██║██║██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗╚██╗ ██╔╝\033[1;m')
                 print('\033[1;33m██║   ██║██║██║        ██║   ██║   ██║██████╔╝ ╚████╔╝\033[1;m')
@@ -198,7 +210,7 @@ while True:
             drawBoard(theBoard)
             move = getPlayerMove(theBoard)
             makeMove(theBoard, player2Letter, move)
-            
+
             if isWinner(theBoard, player2Letter):
                 drawBoard(theBoard)
                 print('────────────────█████████───────────────')
