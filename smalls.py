96dd42ea191dc6f4cb882df7d298b50c25354520
SZkristof <szabokristof0@gmail.com>
2017-03-29 11:41:48 +0200

    colors_finalization

--- smalls.py
+++ smalls.py
@@ -3,42 +3,42 @@ import sys
 import colours
 
 def front_line():
-    line_1 = (colours.red + "████████╗██╗ ██████╗    ████████╗ █████╗  ██████╗    \033[1;32m████████╗ ██████╗ ███████╗")
+    line_1 = (colours.red + "████████╗██╗ ██████╗ " + colours.colour_end + "   ████████╗ █████╗  ██████╗    " + colours.green + "████████╗ ██████╗ ███████╗")
     for x in line_1:
         print(x, end='')
         sys.stdout.flush()
         time.sleep(0.005)
     time.sleep(0.1)
     print("")
-    line_2 = (colours.red + "╚══██╔══╝██║██╔════╝    ╚══██╔══╝██╔══██╗██╔════╝    \033[1;32m╚══██╔══╝██╔═══██╗██╔════╝")
+    line_2 = (colours.red + "╚══██╔══╝██║██╔════╝  " + colours.colour_end + "  ╚══██╔══╝██╔══██╗██╔════╝    " + colours.green + "╚══██╔══╝██╔═══██╗██╔════╝")
     for x in line_2:
         print(x, end='')
         sys.stdout.flush()
         time.sleep(0.005)
     time.sleep(0.1)
     print("")
-    line_3 = (colours.red + "   ██║   ██║██║            ██║   ███████║██║          \033[1;32m  ██║   ██║   ██║█████╗  ")
+    line_3 = (colours.red + "   ██║   ██║██║     " + colours.colour_end + "       ██║   ███████║██║          " + colours.green + "  ██║   ██║   ██║█████╗  ")
     for x in line_3:
         print(x, end='')
         sys.stdout.flush()
         time.sleep(0.005)
     time.sleep(0.1)
     print("")
-    line_4 = (colours.red + "   ██║   ██║██║            ██║   ██╔══██║██║          \033[1;32m  ██║   ██║   ██║██╔══╝  ")
+    line_4 = (colours.red + "   ██║   ██║██║      " + colours.colour_end + "      ██║   ██╔══██║██║          " + colours.green + "  ██║   ██║   ██║██╔══╝  ")
     for x in line_4:
         print(x, end='')
         sys.stdout.flush()
         time.sleep(0.005)
     time.sleep(0.1)
     print("")
-    line_5 = (colours.red + "   ██║   ██║╚██████╗       ██║   ██║  ██║╚██████╗     \033[1;32m  ██║   ╚██████╔╝███████╗")
+    line_5 = (colours.red + "   ██║   ██║╚██████╗   " + colours.colour_end + "    ██║   ██║  ██║╚██████╗     " + colours.green + "  ██║   ╚██████╔╝███████╗")
     for x in line_5:
         print(x, end='')
         sys.stdout.flush()
         time.sleep(0.005)
     time.sleep(0.1)
     print("")
-    line_6 = (colours.red + "   ╚═╝   ╚═╝ ╚═════╝       ╚═╝   ╚═╝  ╚═╝ ╚═════╝     \033[1;32m  ╚═╝    ╚═════╝ ╚══════╝")
+    line_6 = (colours.red + "   ╚═╝   ╚═╝ ╚═════╝    " + colours.colour_end + "   ╚═╝   ╚═╝  ╚═╝ ╚═════╝     " + colours.green + "  ╚═╝    ╚═════╝ ╚══════╝")
     for x in line_6:
         print(x, end='')
         sys.stdout.flush()
